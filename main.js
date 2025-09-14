4be1e51fb4c0b50b3d8a0678d82257b950a54745
IsmailovaAisha <oyshaismailova08@gmail.com>
2025-09-15 00:11:40 +0500

    misolla

---
+++ main.js
@@ -0,0 +1,30 @@
+let word="hello";
+
+for(let i=word.length-1; i>=0; i--){
+    console.log(word[i])
+}
+
+
+let text= "How are you doing today"
+console.log(text.split(" ")+",")
+
+let letters="how are you doing today"
+console.log(letters.split("")+",")
+
+
+let words= "how are you doing today";
+let arr= words.split(" ");
+console.log( arr.length +"ta")
+
+// let world= "salom dunyo";
+
+let matn= "a b c d"
+console.log(matn.replaceAll(" ", ""))
+
+let khasan= "Khasan teacher come back"
+console.log(khasan.replaceAll("a", "o"))
+
+// let back="Khasan teacher came back"
+// let back1=(back.replace("Khasan teacher", " "))
+// console.log(back1+back.replace("back", " "))
+
